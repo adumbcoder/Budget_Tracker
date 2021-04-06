5360f2224a9c479dad43625f93b429286068b082
adumbcoder <70477253+adumbcoder@users.noreply.github.com>
2021-04-05 21:46:52 -0400

    somewhat done

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,31 @@
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/index.html",
+  "/db.js",
+  "/styles.css",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-const FILES_TO_CACHE = [
-
-];
-
 // install the worker
 self.addEventListener("install", function (evt) {
-    // get all image data
-    evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-    ); 
-    //get all static assets
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-    //once its done installing, activate immediately
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    }))
     self.skipWaiting();
-  });
+});
 
 //activate the worker
 self.addEventListener("activate", (evt) => {
@@ -24,15 +33,15 @@ self.addEventListener("activate", (evt) => {
     evt.waitUntil(
       caches.keys().then((keyList) => {
         return Promise.all(
-          keyList.map((key) => {
+          keyList.map((key => {
             if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
               return caches.delete(key);
             }
-          })
-        );
-      })
-    );
-  
+        })
+        )
+        )   
+        })
+    )
     self.clients.claim();
   });
 
